7da95291b87fd3a538f295d540e7d67bc41d8ec2
Benoît Legat <benoit.legat@gmail.com>
2018-04-06 16:13:05 +0200

    Update CI

--- test/polyhedron.jl
+++ test/polyhedron.jl
@@ -6,7 +6,6 @@ include(joinpath(polyhedra_test, "utils.jl"))
 include(joinpath(polyhedra_test, "polyhedra.jl"))
 
 @testset "Polyhedra tests" begin
-    @show lp_solver
     polyhedratest(QHull.QHullLibrary(lp_solver), ["jumpsimplex", "ex1", "infeasible", "nonfulldimensional",
                                                   "doc", "simplex", "permutahedron", "board", "issue48", "empty",
                                                   "orthantdecompose", "largedecompose"])
