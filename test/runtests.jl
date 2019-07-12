0eded3c2e1d0ba7a29935798b531d7893af8f7a1
Benoît Legat <benoit.legat@gmail.com>
2019-07-12 15:37:02 +0200

    Fix qhull with VRep

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,21 +1,25 @@
 using Test
 import QHull
 
-pts = [-1.0 0;
-       1 1;
-       3 0;
-       3 3;
-       2 2;
-       0 3;
-       -1 2]
+@testset "chull" begin
+    pts = [-1.0 0;
+           1 1;
+           3 0;
+           3 3;
+           2 2;
+           0 3;
+           -1 2]
 
-hull = QHull.chull(pts)
-@test hull.vertices == [1, 3, 4, 6, 7]
-@test size(hull.points) == size(pts)
-@test hull.simplices == Array{Int,1}[[3,1], [4,3], [6,4], [7,1], [7,6]]
+    hull = QHull.chull(pts)
+    @test hull.vertices == [1, 3, 4, 6, 7]
+    @test size(hull.points) == size(pts)
+    @test hull.simplices == Array{Int,1}[[3,1], [4,3], [6,4], [7,1], [7,6]]
 
-# multi-dim
-x = randn(1000, 5)
-hull = QHull.chull(x)
+    # multi-dim
+    x = randn(1000, 5)
+    hull = QHull.chull(x)
+end
 
-include("polyhedron.jl")
+@testset "Polyhedra tests" begin
+    include("polyhedron.jl")
+end
